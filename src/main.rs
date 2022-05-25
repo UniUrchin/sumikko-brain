a5025b5f6d504d30fde98edf7284dc398d6c5e16
YowaPenguin <85057870+YowaPenguin@users.noreply.github.com>
2022-05-25 23:55:31 +0900

    can read testfile

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::fs;
+
+use anyhow::{Context, Result as AResult};
+
+fn main() -> AResult<()> {
+    let filepath = env::args()
+        .nth(1)
+        .with_context(|| "引数がおかしいんだよ〜!!")?;
+    let contents = fs::read_to_string(filepath)?;
+
+    println!("{}", contents);
+
+    return Ok(())
 }
